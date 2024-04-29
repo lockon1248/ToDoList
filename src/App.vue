875e30268e6929c2a64fefd6789c4794f410b2ec
lockon <lockon5445@gmail.com>
2024-04-29 15:43:36 +0800

    feat: 新增多語系翻譯以及代辦事項樣式調整:
    1. 新增多語系相關選項，新增英、簡繁中語系切換及相關內容
    2. 調整代辦事項樣式

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,48 @@
 <template>
-	<div class="min-w-[500px] max-w-[50vw] h-[75vh] mx-auto bg-tiffanyBlue rounded-lg border border-white">
-		<header class="pt-5 text-center">To Do List</header>
-		<div class="flex mx-auto gap-[10px] min-w-[200px] max-w-[300px] pt-[50px]">
-			<label for="" class="flex leading-6 items-center">title:</label>
-			<a-input v-model:value="inputVal" placeholder="輸入內容" />
-			<a-button @click="joinList">join</a-button>
-		</div>
-		<div class="mx-auto flex mt-[20px] min-w-[200px] max-w-[300px] flex-col">
-			<div class="flex p-2 shadow-lg rounded-xl gap-1 items-center" v-for="(item, index) in toDoList" :key="index">
-				<a-input :bordered="false" :value="item.name" :readonly="!item.edit" @input="handleInput(item, $event)" class="p-[5px]" />
-				<font-awesome-icon :icon="item.edit ? 'fa-solid fa-floppy-disk' : 'fa-solid fa-pen-to-square'" class="cursor-pointer text-gray-500" @click="edit(item)" />
-				<font-awesome-icon icon="fa-solid fa-trash" class="cursor-pointer text-gray-500" @click="deleteItem(item)" />
+	<a-config-provider :autoInsertSpaceInButton="false">
+		<div class="min-w-[300px] max-w-[50vw] h-[75vh] mx-auto flex flex-col gap-4 shadow-lg bg-tiffanyBlue rounded-lg border border-white">
+			<header class="pt-5 text-center gap-[10px] text-[20px] font-black">{{ $t('toDoList') }}</header>
+			<div class="text-center flex gap-[10px] items-center mx-4 flex-wrap" @change="openChange">
+				<p class="flex flex-nowrap font-semibold">{{ $t('language_switch') }}:</p>
+				<a-radio-group v-model:value="langValue">
+					<a-radio-button class="bg-transparent" v-for="item in langSelect" :key="item.val" :value="item.val">{{ item.lang }}</a-radio-button>
+				</a-radio-group>
+			</div>
+			<div class="flex gap-[10px] mx-4">
+				<p class="flex leading-6 items-center whitespace-nowrap font-semibold">{{ $t('title') }}:</p>
+				<a-input v-model:value="inputVal" :placeholder="$t('inputContent')" />
+				<a-button class="shadow-lg border-transparent" @click="joinList">{{ $t('join') }}</a-button>
+			</div>
+			<div class="flex my-[10px] flex-col flex-grow">
+				<div class="flex p-2 shadow-lg rounded-xl gap-1 items-center min-w-[275px] mx-3 my-1 overflow-auto" v-for="(item, index) in toDoList" :key="index">
+					<a-input class="p-[5px]" :bordered="false" :value="item.name" :readonly="!item.edit" @input="handleInput(item, $event)" />
+					<font-awesome-icon class="cursor-pointer text-gray-500" :icon="item.edit ? 'fa-solid fa-floppy-disk' : 'fa-solid fa-pen-to-square'" @click="edit(item)" />
+					<font-awesome-icon class="cursor-pointer text-gray-500" icon="fa-solid fa-trash" @click="deleteItem(item)" />
+				</div>
 			</div>
 		</div>
-	</div>
+	</a-config-provider>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { i18n } from '@/utils/i18n';
 const inputVal = ref('');
 const toDoList = ref([]);
+const langSelect = computed(() => [
+	{ lang: i18n.global.t('en'), val: 'en' },
+	{ lang: i18n.global.t('tc'), val: 'zh_TW' },
+	{ lang: i18n.global.t('sc'), val: 'zh_CN' }
+]);
+const langValue = ref(getLangValue());
+function getLangValue() {
+	const lang = document.body.lang;
+	// 遍历 langSelect 数组，寻找匹配的语言选项
+	for (let i = 0; i < langSelect.value.length; i++) {
+		if (langSelect.value[i].val === lang) {
+			return langSelect.value[i].val;
+		}
+	}
+}
 const getList = () => {
 	const toDoListString = localStorage.getItem('toDoList');
 	if (!toDoListString) {
@@ -30,7 +54,8 @@ const getList = () => {
 getList();
 const joinList = () => {
 	if (inputVal.value.trim() === '') {
-		alert('未輸入訊息');
+		let message = i18n.global.t('messageNotEntered');
+		alert(message);
 		console.log(toDoList.value.length);
 		return;
 	} else {
@@ -55,7 +80,8 @@ const deleteItem = (item) => {
 const edit = (item) => {
 	if (item.edit) {
 		if (item.name === '') {
-			alert('修改失敗你沒寫字');
+			let message = i18n.global.t('failedToModify');
+			alert(message);
 			getList();
 		} else {
 			item.edit = false;
@@ -72,4 +98,16 @@ const edit = (item) => {
 const handleInput = (item, event) => {
 	item.name = event.target.value;
 };
+const openChange = () => {
+	i18n.global.locale.value = langValue.value;
+};
 </script>
+<style lang="scss" scoped>
+:where(.css-dev-only-do-not-override-1hsjdkk).ant-btn-default:not(:disabled):hover {
+	color: #000000;
+	border: transparent;
+}
+:where(.css-dev-only-do-not-override-1hsjdkk).ant-radio-button-wrapper-checked:not(.ant-radio-button-wrapper-disabled) {
+	background-color: transparent;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 /* v8 ignore start */
 import { createApp } from 'vue';
 import App from './App.vue';
+import { i18n } from '@/utils/i18n';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faTrash, faPenToSquare, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+
 import '@/assets/styles/main.css';
 library.add(faTrash, faPenToSquare, faFloppyDisk);
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+createApp(App).use(i18n).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
