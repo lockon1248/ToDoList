c5594067896799698fd7b62c8cb8db4322a2bef2
lockon <lockon5445@gmail.com>
2025-12-09 15:00:59 +0800

    feat: 更改版號以及更新 vite 和 vue 套件
    1. 將版號變為1.0.0
    2. 更新 vue 和 vite 套件版本
    3. 將 viteCompression2 換回 viteCompression 避免套件衝突

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import { compression } from 'vite-plugin-compression2';
+import viteCompression from "vite-plugin-compression";
 import Components from 'unplugin-vue-components/vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
@@ -13,7 +13,7 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		vueJsx(),
-		compression(), // vite 的 build 包體積壓縮工具
+		viteCompression(), // vite 的 build 包體積壓縮工具
 		Components({
 			resolvers: [
 				AntDesignVueResolver({
